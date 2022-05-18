59083f79fdb1b82d6a97f39ea5f0e905d62eaa6a
MPilot93 <marco.plt.93@gmail.com>
2022-05-18 14:31:12 +0200

    pareggio

--- game.js
+++ game.js
@@ -1,12 +1,14 @@
 var line
 var column
 var turn
+var turnTot=1
 var n
+var win=0
 
    var i
    var grid=[3]
    for(i=0;i<3;i++)  
-   a[i]=[3]
+   grid[i]=[3]
 
   function play()
   {  
@@ -28,8 +30,10 @@ if(turn==true)
 grid[line][column]=0
 grid[n]=0
 document.images[n].src="o.png"
-document.onclick=false
-//document.getElementById("name").disabled=true
+
+//grid[line][column].isclicked=false
+//document.onclick=false
+document.getElementsByClassName("Myimg").disabled=true
 
 
 control(0)
@@ -40,11 +44,16 @@ else
 grid[line][column]=1
 grid[n]=1
 document.images[n].src="x.png"
-document.onclick=false
+
+document.getElementsByClassName("Myimg").disabled=true
+//grid[line][column].isclicked=false
+//document.onclick=false
 //document.getElementById("name").disabled=true
 control(1)
 }
-turn=!turn    
+
+turn=!turn 
+turnTot++   
 }
 
 
@@ -53,41 +62,50 @@ turn=!turn
 {
     if ((grid[0]==0)&&(grid[1]==0)&&(grid[2]==0))
 {
+   win=1
     alert("ha vinto il cerchio")
+    
 }
 
 if ((grid[3]==0)&&(grid[4]==0)&&(grid[5]==0))
 {
+   win=1
     alert ("ha vinto il cerchio")
 }
 
 if ((grid[6]==0)&&(grid[7]==0)&&(grid[8]==0))
 {
+   win=1
     alert ("ha vinto il cerchio")
 }
 
 if ((grid[0]==0)&&(grid[3]==0)&&(grid[6]==0))
 {
+   win=1
     alert ("ha vinto il cerchio")
 }
 
 if ((grid[1]==0)&&(grid[4]==0)&&(grid[7]==0))
 {
+   win=1
     alert ("ha vinto il cerchio")
 }
 
 if ((grid[2]==0)&&(grid[5]==0)&&(grid[8]==0))
 {
+   win=1
     alert ("ha vinto il cerchio")
 }
 
 if ((grid[0]==0)&&(grid[4]==0)&&(grid[8]==0))
 {
+   win=1
     alert ("ha vinto il cerchio")
 }
 
 if ((grid[2]==0)&&(grid[4]==0)&&(grid[6]==0))
 {
+   win=1
     alert ("ha vinto il cerchio")
 }
 
@@ -96,56 +114,73 @@ if ((grid[2]==0)&&(grid[4]==0)&&(grid[6]==0))
 
 if ((grid[0]==1)&&(grid[1]==1)&&(grid[2]==1))
 {
+   win=1
     alert ("ha vinto la croce")
 }
 
 
 if ((grid[3]==1)&&(grid[4]==1)&&(grid[4]==1))
 {
+   win=1
     alert ("ha vinto la croce")
 }
 
 
 if ((grid[6]==1)&&(grid[7]==1)&&(grid[8]==1))
 {
+   win=1
     alert ("ha vinto la croce")
 }
 
 
 if ((grid[0]==1)&&(grid[3]==1)&&(grid[6]==1))
 {
+   win=1
    alert ("ha vinto la croce")
 }
 
 if ((grid[1]==1)&&(grid[4]==1)&&(grid[7]==1))
 {
+   win=1
     alert ("ha vinto la croce")
 }
 
 
 if ((grid[2]==1)&&(grid[5]==1)&&(grid[8]==1))
 {
+   win=1
     alert ("ha vinto la croce")
 }
 
 
 if ((grid[2]==1)&&(grid[4]==1)&&(grid[8]==1))
 {
+   win=1
     alert ("ha vinto la croce")
 }
 
 if ((grid[2]==1)&&(grid[4]==1)&&(grid[6]==1))
 {
+   win=1
     alert ("ha vinto la croce")
+}
+if(turnTot==9 && win==0){
+   alert ("la partita si è conclusa in pareggio")
 }
 }  
 
 
 function NewGame(){
-   for(var i=0; i<9;i++){
-      document.images[i].src="nothing.png"
-      grid[i]=-1
+   for(let j=0; j<9;j++){
+      document.images[j].src="nothing.png"
+      grid[j]=-1
    }
   
    turn==!turn
+   turnTot=1
+}
+
+function disable(n){
+   var temp=document.getElementsByName(toString(n))
+   temp.disable=true
 }
